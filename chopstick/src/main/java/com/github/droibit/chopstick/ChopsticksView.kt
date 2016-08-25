39e29d627448d91347e6ff6e68bc664faba11729
Shinya Kumagai <roomful.rooms@gmail.com>
2016-08-25 23:02:27 +0900

    Added findView of extension methods

--- chopstick/src/main/java/com/github/droibit/chopstick/ChopsticksView.kt
+++ chopstick/src/main/java/com/github/droibit/chopstick/ChopsticksView.kt
@@ -7,26 +7,32 @@ import android.support.v4.app.Fragment as SupportFragment
 import android.support.v7.widget.RecyclerView.ViewHolder
 import android.view.View
 
-fun <V : View> Activity.bindView(@IdRes id: Int): Lazy<V> = require(id, viewFinder)
-fun <V : View> Fragment.bindView(@IdRes id: Int): Lazy<V> = require(id, viewFinder)
-fun <V : View> SupportFragment.bindView(@IdRes id: Int): Lazy<V> = require(id, viewFinder)
-fun <V : View> ViewHolder.bindView(@IdRes id: Int): Lazy<V> = require(id, viewFinder)
-fun <V : View> View.bindView(@IdRes id: Int): Lazy<V> = require(id, viewFinder)
+fun <V : View> Activity.findView(@IdRes id: Int): V = required(id, viewFinder)
+fun <V : View> Fragment.findView(@IdRes id: Int): V = required(id, viewFinder)
+fun <V : View> SupportFragment.findView(@IdRes id: Int): V = required(id, viewFinder)
+fun <V : View> ViewHolder.findView(@IdRes id: Int): V = required(id, viewFinder)
+fun <V : View> View.findView(@IdRes id: Int): V = required(id, viewFinder)
+
+fun <V : View> Activity.bindView(@IdRes id: Int): Lazy<V> = requiredLazy(id, viewFinder)
+fun <V : View> Fragment.bindView(@IdRes id: Int): Lazy<V> = requiredLazy(id, viewFinder)
+fun <V : View> SupportFragment.bindView(@IdRes id: Int): Lazy<V> = requiredLazy(id, viewFinder)
+fun <V : View> ViewHolder.bindView(@IdRes id: Int): Lazy<V> = requiredLazy(id, viewFinder)
+fun <V : View> View.bindView(@IdRes id: Int): Lazy<V> = requiredLazy(id, viewFinder)
 
 fun <V : View> Activity.bindViews(vararg ids: Int): Lazy<List<V>>
-        = require(ids) { findViewById(it) }
+        = requiredLazy(ids) { findViewById(it) }
 
 fun <V : View> Fragment.bindViews(vararg ids: Int): Lazy<List<V>>
-        = require(ids) { view?.findViewById(it) }
+        = requiredLazy(ids) { view?.findViewById(it) }
 
 fun <V : View> SupportFragment.bindViews(vararg ids: Int): Lazy<List<V>>
-        = require(ids) { view?.findViewById(it) }
+        = requiredLazy(ids) { view?.findViewById(it) }
 
 fun <V : View> ViewHolder.bindViews(vararg ids: Int): Lazy<List<V>>
-        = require(ids) { itemView.findViewById(it) }
+        = requiredLazy(ids) { itemView.findViewById(it) }
 
 fun <V : View> View.bindViews(vararg ids: Int): Lazy<List<V>>
-        = require(ids) { findViewById(it) }
+        = requiredLazy(ids) { findViewById(it) }
 
 internal val Activity.viewFinder: (Int) -> View?
     get() = { findViewById(it) }
@@ -44,11 +50,16 @@ internal val View.viewFinder: (Int) -> View?
     get() = { findViewById(it) }
 
 @Suppress("UNCHECKED_CAST")
-private inline fun <V : View> require(id: Int, crossinline finder: (Int) -> View?)
+private inline fun <V : View> requiredLazy(id: Int, crossinline finder: (Int) -> View?)
         = lazy { requireNotNull(finder(id)) { "Not found view by id($id)." } as V }
 
 @Suppress("UNCHECKED_CAST")
-private inline fun <V : View> require(ids: IntArray, crossinline finder: (Int) -> View?) = lazy {
+private inline fun <V : View> requiredLazy(ids: IntArray, crossinline finder: (Int) -> View?) = lazy {
     require(ids.size > 0) { "ids size > 0" }
     ids.map { requireNotNull(finder(it)) { "Not found view by id($it)." } as V }.toList()
+}
+
+@Suppress("UNCHECKED_CAST")
+private inline fun <V : View> required(id: Int, crossinline finder: (Int) -> View?): V {
+    return requireNotNull(finder(id)) { "Not found view by id($id)." } as V
 }
